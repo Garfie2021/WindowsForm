1ea4771162fd99a7fd74fa12d04763b80acd05ae
キャベツウニ <akihitoukami@gmail.com>
2022-03-03 14:21:01 +0900

    1

---
+++ Src_Static/WindowsFormsApplication1/C3.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    // staticメソッド群を、マルチスレッドで実行するためのメソッド群
+    public class CVariable実験1
+    {
+        // メソッド内の変数を使ってループするメソッド
+        public void VariableTest1(object id)
+        {
+            CVariable実験2.VariableTest1(id);
+        }
+
+        // クラスのメンバ変数を使ってループするメソッド
+        public void VariableTest2(object id)
+        {
+            CVariable実験2.VariableTest2(id);
+        }
+    }
+
+    // staticメソッド群
+    public static class CVariable実験2
+    {
+        private static long mCnt = 0;
+
+        // メソッド内の変数を使ってループするメソッド
+        public static void VariableTest1(object id)
+        {
+            string FileName = "result_" + id + ".txt";
+
+            if (File.Exists(FileName)) File.Delete(FileName);
+
+            long iCnt = 0;
+            string tmp = null;
+
+            while (iCnt < 1000)
+            {
+                tmp = id + " " + iCnt.ToString() + "\r\n";
+
+                System.Diagnostics.Debug.WriteLine(tmp);
+                File.AppendAllText(FileName, tmp);
+
+                iCnt++;
+            }
+        }
+
+        // クラスのメンバ変数を使ってループするメソッド
+        public static void VariableTest2(object id)
+        {
+            string FileName = "result_" + id + ".txt";
+
+            if (File.Exists(FileName)) File.Delete(FileName);
+
+            string tmp = null;
+            mCnt = 0;
+
+            while (mCnt < 1000)
+            {
+                tmp = id + " " + mCnt.ToString() + "\r\n";
+
+                System.Diagnostics.Debug.WriteLine(tmp);
+                File.AppendAllText(FileName, tmp);
+
+                mCnt++;
+            }
+        }
+    }
+}
---
+++ Src_Static/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        static Dictionary<string, string> mDict = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //実証実験1();
+            //実証実験2();
+            //実証実験3();
+            実証実験4();
+        }
+
+        // メソッド内の変数でループするstaticメソッドを、シングルスレッドで実行する。
+        private void 実証実験1()
+        {
+            var Variable実験 = new CVariable実験1();
+            Variable実験.VariableTest1("A");
+            Variable実験.VariableTest1("B");
+            Variable実験.VariableTest1("C");
+        }
+
+        // メソッド内の変数でループするstaticメソッドを、マルチスレッドで実行する。
+        private void 実証実験2()
+        {
+            var Variable実験 = new CVariable実験1();
+            var i1 = new Thread(new ParameterizedThreadStart(Variable実験.VariableTest1));
+            var t2 = new Thread(new ParameterizedThreadStart(Variable実験.VariableTest1));
+            var t3 = new Thread(new ParameterizedThreadStart(Variable実験.VariableTest1));
+
+            i1.Start("A");
+            t2.Start("B");
+            t3.Start("C");
+        }
+
+        // クラスのstaticメンバ変数でループするstaticメソッドを、シングルスレッドで実行する。
+        private void 実証実験3()
+        {
+            var Variable実験 = new CVariable実験1();
+            Variable実験.VariableTest2("A");
+            Variable実験.VariableTest2("B");
+            Variable実験.VariableTest2("C");
+        }
+
+        // クラスのメンバ変数でループするメソッドを、マルチスレッドで実行する。
+        private void 実証実験4()
+        {
+            var Variable実験 = new CVariable実験1();
+            var i1 = new Thread(new ParameterizedThreadStart(Variable実験.VariableTest2));
+            var t2 = new Thread(new ParameterizedThreadStart(Variable実験.VariableTest2));
+            var t3 = new Thread(new ParameterizedThreadStart(Variable実験.VariableTest2));
+
+            i1.Start("A");
+            t2.Start("B");
+            t3.Start("C");
+        }
+
+    }
+}
