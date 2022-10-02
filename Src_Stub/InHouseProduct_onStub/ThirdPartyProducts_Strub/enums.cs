39b8f6f2a60d5476cbe08f985926066ab730b043
ukami <ukami@interserve.co.jp>
2022-10-02 19:31:19 +0900

    1

---
+++ Src_Stub/InHouseProduct_onStub/ThirdPartyProducts_Strub/enums.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdPartyProducts
+{
+    public enum Status : uint
+    {
+        /// <summary>
+        /// 未起動
+        /// </summary>
+        NotStarted = 0x00000000,
+
+        /// <summary>
+        /// 起動済み
+        /// </summary>
+        Started = 0x00000001,
+
+        /// <summary>
+        /// 処理１は終了している
+        /// </summary>
+        Transaction1_End = 0x00000002,
+
+        /// <summary>
+        /// 処理２は終了している
+        /// </summary>
+        Transaction2_End = 0x00000003,
+    }
+
+    public enum ReturnCode : uint
+    {
+        /// <summary>
+        /// 正常終了
+        /// </summary>
+        Success = 0x00000000,
+
+        /// <summary>
+        /// 異常終了
+        /// </summary>
+        Error = 0x00000010,
+    }
+}
