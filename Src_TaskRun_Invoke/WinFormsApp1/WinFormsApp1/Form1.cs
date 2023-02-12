91a7f1717a117e62f85a647015bd1991b3d8458d
ukami <ukami@interserve.co.jp>
2023-02-12 15:19:23 +0900

    1

--- Src_TaskRun_Invoke/WinFormsApp1/WinFormsApp1/Form1.cs
+++ Src_TaskRun_Invoke/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -6,5 +6,30 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "aaaaa";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                textBox1.Text = "bbbbb";
+            });
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                var value = "ccccc";
+                Invoke(() =>
+                {
+                    textBox1.Text = value;
+                });
+            });
+        }
     }
 }
