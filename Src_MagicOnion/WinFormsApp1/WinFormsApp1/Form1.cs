11c5b5449731276cf71ba59394813f57ad9acbdb
ukami <ukami@interserve.co.jp>
2023-02-19 18:07:55 +0900

    1

--- Src_MagicOnion/WinFormsApp1/WinFormsApp1/Form1.cs
+++ Src_MagicOnion/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -1,3 +1,7 @@
+using Grpc.Net.Client;
+using MagicOnion.Client;
+using WebApplication1.Shared;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -6,5 +10,19 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // Connect to the server using gRPC channel.
+            var channel = GrpcChannel.ForAddress("https://localhost:7120");
+
+            // Create a proxy to call the server transparently.
+            var client = MagicOnionClient.Create<IMyFirstService>(channel);
+
+            // Call the server-side method using the proxy.
+            var result = (await client.SumAsync(123, 456));
+            textBox1.Text = result.ToString();
+        }
+
     }
 }
