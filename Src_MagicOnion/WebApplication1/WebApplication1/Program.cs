4d18dd44f4c15671b68e11aca371c2dc0c8797a3
ukami <ukami@interserve.co.jp>
2023-02-19 17:52:14 +0900

    1

--- Src_MagicOnion/WebApplication1/WebApplication1/Program.cs
+++ Src_MagicOnion/WebApplication1/WebApplication1/Program.cs
@@ -1,5 +1,14 @@
+using MagicOnion;
+using MagicOnion.Server;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+builder.Services.AddMagicOnion();
+
 var app = builder.Build();
+
+app.MapMagicOnionService();
 
 app.MapGet("/", () => "Hello World!");
 
