e4ed759f0033c207c05039ec3865aefef3969038
ukami <ukami@interserve.co.jp>
2023-05-15 14:06:08 +0900

    1

---
+++ Src_MagicOnion_v4/ClassLibrary1/ClassLibrary1/Formatter/ColorFormatter.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack;
+using MessagePack.Formatters;
+using System.Drawing;
+
+namespace ClassLibrary1.Interface
+{
+    public class ColorFormatter : IMessagePackFormatter<Color>
+    {
+        public static readonly IMessagePackFormatter<Color> Instance = new ColorFormatter();
+
+        public void Serialize(ref MessagePackWriter writer, Color value, MessagePackSerializerOptions options)
+        {
+            writer.WriteInt32(value.ToArgb());
+        }
+
+        public Color Deserialize(ref MessagePackReader reader, MessagePackSerializerOptions options)
+        {
+            return Color.FromArgb(reader.ReadInt32());
+        }
+    }
+}
---
+++ Src_MagicOnion_v4/WinFormsApp1/WinFormsApp1/Program.cs
@@ -0,0 +1,31 @@
+using ClassLibrary1.Interface;
+using MessagePack.Resolvers;
+using MessagePack;
+
+namespace WinFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            var Resolver = CompositeResolver.Create(
+                NativeDateTimeResolver.Instance,
+                NativeDecimalResolver.Instance,
+                DynamicGenericResolver.Instance,
+                ColorResolver.Instance,
+                ContractlessStandardResolver.Instance
+            );
+            var options = ContractlessStandardResolver.Options.WithResolver(Resolver);
+            MessagePackSerializer.DefaultOptions = options;
+
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
