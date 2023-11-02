7ce1f7e688166735a8c921c1405bb6a29cf7bf3b
ukami <ukami@interserve.co.jp>
2023-11-02 20:50:23 +0900

    1

---
+++ Src_ActiveReports/SectionReportApplication1/SectionReportApplication1/SectionReport1.cs
@@ -0,0 +1,20 @@
+﻿namespace SectionReportApplication1
+{
+    /// <summary>
+    /// NewActiveReport1 の概要の説明です。
+    /// </summary>
+    public partial class SectionReport1 : GrapeCity.ActiveReports.SectionReport
+    {
+
+        public SectionReport1()
+        {
+            //
+            // デザイナー サポートに必要なメソッドです。このメソッドの内容を
+            //
+            InitializeComponent();
+
+            textBox1.Text = "test1";
+
+        }
+    }
+}
---
+++ Src_ActiveReports/SectionReportApplication1/WinFormsApp1/Form1.cs
@@ -0,0 +1,17 @@
+using SectionReportApplication1;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            viewer1.LoadDocument(new SectionReport1());
+        }
+    }
+}
