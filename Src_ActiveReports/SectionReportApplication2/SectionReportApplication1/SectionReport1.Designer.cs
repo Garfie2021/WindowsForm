96e8b0ec8f0452ff75843983c959db83d691c4ff
ukami <ukami@interserve.co.jp>
2023-11-04 00:11:12 +0900

    1

---
+++ Src_ActiveReports/SectionReportApplication2/SectionReportApplication1/SectionReport1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SectionReportApplication1
+{
+    /// <summary>
+    /// NewActiveReport1 の概要の説明です。
+    /// </summary>
+    partial class SectionReport1
+    {
+        private GrapeCity.ActiveReports.SectionReportModel.PageHeader pageHeader;
+        private GrapeCity.ActiveReports.SectionReportModel.Detail detail;
+        private GrapeCity.ActiveReports.SectionReportModel.PageFooter pageFooter;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+            }
+            base.Dispose(disposing);
+        }
+
+        #region ActiveReports デザイナーで生成されたコード
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(SectionReport1));
+            this.pageHeader = new GrapeCity.ActiveReports.SectionReportModel.PageHeader();
+            this.detail = new GrapeCity.ActiveReports.SectionReportModel.Detail();
+            this.textBox1 = new GrapeCity.ActiveReports.SectionReportModel.TextBox();
+            this.pageFooter = new GrapeCity.ActiveReports.SectionReportModel.PageFooter();
+            ((System.ComponentModel.ISupportInitialize)(this.textBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // pageHeader
+            // 
+            this.pageHeader.Controls.AddRange(new GrapeCity.ActiveReports.SectionReportModel.ARControl[] {
+            this.textBox1});
+            this.pageHeader.Name = "pageHeader";
+            // 
+            // detail
+            // 
+            this.detail.Name = "detail";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Height = 0.2F;
+            this.textBox1.Left = 0.229F;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Text = "textBox1";
+            this.textBox1.Top = 0.04200001F;
+            this.textBox1.Width = 1F;
+            // 
+            // pageFooter
+            // 
+            this.pageFooter.Name = "pageFooter";
+            // 
+            // SectionReport1
+            // 
+            this.MasterReport = false;
+            this.PageSettings.PaperHeight = 11F;
+            this.PageSettings.PaperWidth = 8.5F;
+            this.Sections.Add(this.pageHeader);
+            this.Sections.Add(this.detail);
+            this.Sections.Add(this.pageFooter);
+            this.StyleSheet.Add(new DDCssLib.StyleSheetRule("font-style: normal; text-decoration: none; font-weight: normal; font-size: 10pt; " +
+            "color: Black; font-family: \"MS UI Gothic\"; ddo-char-set: 128", "Normal"));
+            this.StyleSheet.Add(new DDCssLib.StyleSheetRule("font-size: 16pt; font-weight: bold; font-family: \"MS UI Gothic\"; ddo-char-set: 12" +
+            "8", "Heading1", "Normal"));
+            this.StyleSheet.Add(new DDCssLib.StyleSheetRule("font-size: 14pt; font-weight: bold; font-style: inherit; font-family: \"MS UI Goth" +
+            "ic\"; ddo-char-set: 128", "Heading2", "Normal"));
+            this.StyleSheet.Add(new DDCssLib.StyleSheetRule("font-size: 13pt; font-weight: bold; ddo-char-set: 128", "Heading3", "Normal"));
+            ((System.ComponentModel.ISupportInitialize)(this.textBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+        #endregion
+
+        private GrapeCity.ActiveReports.SectionReportModel.TextBox textBox1;
+    }
+}
