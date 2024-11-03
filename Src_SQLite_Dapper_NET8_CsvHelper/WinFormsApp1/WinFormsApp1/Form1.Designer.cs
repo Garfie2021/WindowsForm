7f24a6c9d8884cb0e64c8822fe444b9296d9e123
testUser1 <testUser1@a.com>
2024-11-03 14:34:44 +0900

    1

---
+++ Src_SQLite_Dapper_NET8_CsvHelper/WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn追加 = new Button();
+            dataGridView1 = new DataGridView();
+            Clm編集 = new DataGridViewButtonColumn();
+            Clm削除 = new DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // btn追加
+            // 
+            btn追加.Location = new Point(12, 12);
+            btn追加.Name = "btn追加";
+            btn追加.Size = new Size(75, 23);
+            btn追加.TabIndex = 0;
+            btn追加.Text = "追加";
+            btn追加.UseVisualStyleBackColor = true;
+            btn追加.Click += btn追加_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Clm編集, Clm削除 });
+            dataGridView1.Location = new Point(12, 41);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Size = new Size(776, 397);
+            dataGridView1.TabIndex = 1;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Clm編集
+            // 
+            Clm編集.FillWeight = 50F;
+            Clm編集.HeaderText = "";
+            Clm編集.Name = "Clm編集";
+            Clm編集.Text = "編集";
+            Clm編集.UseColumnTextForButtonValue = true;
+            Clm編集.Width = 50;
+            // 
+            // Clm削除
+            // 
+            Clm削除.HeaderText = "";
+            Clm削除.Name = "Clm削除";
+            Clm削除.Text = "削除";
+            Clm削除.UseColumnTextForButtonValue = true;
+            Clm削除.Width = 50;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(btn追加);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btn追加;
+        private DataGridView dataGridView1;
+        private DataGridViewButtonColumn Clm編集;
+        private DataGridViewButtonColumn Clm削除;
+    }
+}
---
+++ Src_SQLite_Dapper_NET8_CsvHelper/WinFormsApp1/WinFormsApp1/Sql/Sql政権.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using Maintenance.Common;
+using Maintenance.Model;
+using System.Data.SQLite;
+
+namespace Maintenance.Sql
+{
+    public static class Sql政権
+    {
+        public static List<M政権> SelectAll()
+        {
+            using (var sqliteConnection = new SQLiteConnection(StaticData.ConnectionString))
+            {
+                const string query = "SELECT * FROM 政権";
+
+                sqliteConnection.Open();
+                return sqliteConnection.Query<M政権>(query).AsList();
+            }
+        }
+
+        public static M政権? SelectById(M政権 m政権)
+        {
+            using (var sqliteConnection = new SQLiteConnection(StaticData.ConnectionString))
+            {
+                const string query = "SELECT * FROM 政権 WHERE Id = @Id";
+
+                sqliteConnection.Open();
+                return sqliteConnection.QueryFirst<M政権>(query, m政権);
+            }
+        }
+
+        public static void Add(M政権 m政権)
+        {
+            using (var sqliteConnection = new SQLiteConnection(StaticData.ConnectionString))
+            {
+                const string query = "INSERT INTO 政権 (Start, End, Name) VALUES (@Start, @End, @Name)";
+
+                sqliteConnection.Open();
+                sqliteConnection.Execute(query, m政権);
+            }
+        }
+
+        public static void Update(M政権 m政権)
+        {
+            using (var sqliteConnection = new SQLiteConnection(StaticData.ConnectionString))
+            {
+                const string query = "UPDATE 政権 SET Start = @Start, End = @End, Name = @Name WHERE Id = @Id";
+
+                sqliteConnection.Open();
+                sqliteConnection.Execute(query, m政権);
+            }
+        }
+
+        public static void Delete(M政権 m政権)
+        {
+            using (var sqliteConnection = new SQLiteConnection(StaticData.ConnectionString))
+            {
+                const string query = "DELETE FROM 政権 WHERE Id = @Id";
+
+                sqliteConnection.Open();
+                sqliteConnection.Execute(query, m政権);
+            }
+        }
+
+    }
+}
